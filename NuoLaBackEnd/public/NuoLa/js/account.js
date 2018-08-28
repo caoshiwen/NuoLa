0570c3a20cd3a44a9cd47fa31cf7595e300dc582
csw <csw199502@outlook.com>
2018-08-28 17:43:14 +0800

    2018/8/28

---
+++ NuoLaBackEnd/public/NuoLa/js/account.js
@@ -0,0 +1,19 @@
+let a;
+initDate();
+
+
+let vm = new Vue({
+    el: "#wrap",
+    data() {
+        return {
+        }
+    },
+    components: {
+        "common-nav": common_nav,
+        "common-footer": common_footer,
+    }
+});
+
+function initDate() {
+
+}
